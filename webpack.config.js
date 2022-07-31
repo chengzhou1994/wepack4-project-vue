bc5d7a5ce11afe0386cbc4c558520e8085383f34
成舟 <1738248438@qq.com>
2022-07-31 17:15:56 +0800

    使用html-webpack-plugin插件来将HTML引用路径和我们的构建结果关联起来

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path') //引入核心内置模块path,用户获取文件路径等
+// 我们不希望打包一次，就新建一次html文件来引用打包后的文件，这样显得不智能或者说当你打包的文件名修改后，引用路径就会出错。
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   mode: 'production',
   entry: './src/main.js', // 打包入口：指示 webpack 应该使用哪个模块，来作为构建其内部依赖图的开始
@@ -9,5 +11,11 @@ module.exports = {
   resolve: {}, // 配置解析：配置别名、extensions 自动解析确定的扩展等等
   devServer: {}, // 开发服务器：run dev/start 的配置，如端口、proxy等
   module: {}, // 模块配置：配置loader（处理非 JavaScript 文件，比如 less、sass、jsx、图片等等）等
-  plugins: [] // 插件的配置：打包优化、资源管理和注入环境变量
+  // 插件的配置：打包优化、资源管理和注入环境变量
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html', // 配置输出文件名和路径
+      template: './public/index.html' //设置模板
+    })
+  ]
 }
